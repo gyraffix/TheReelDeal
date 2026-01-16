ec5dd89053853ed9cc3ba801f771252e38cffea5
Dylan Valkenaar <valkdylan@gmail.com>
2026-01-16 11:31:33 +0100

    Added wandering fish prefab.
    Needs to be placed on navmesh
    also made playing with controller possible(kinda)

---
+++ Assets/Scripts/WanderingFish.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class WanderingFish : MonoBehaviour
+{
+    public float radius;
+    public float wanderTimer;
+
+    private NavMeshAgent agent;
+    private float timer;
+    private float offset;
+    
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        offset = Random.value;
+        agent = GetComponent<NavMeshAgent>();
+        wanderTimer = wanderTimer + offset;
+        timer = wanderTimer;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer > wanderTimer)
+        {
+            Vector3 newTarget = RandomSpherePos(transform.position, radius, -1);
+            agent.SetDestination(newTarget);
+            timer = 0;
+            agent.gameObject.transform.LookAt(newTarget);
+        }
+    }
+
+    public static Vector3 RandomSpherePos(Vector3 origin, float dist, int layerMask)
+    {
+        Vector3 randDirection = Random.insideUnitSphere * dist;
+
+        randDirection += origin;
+
+        NavMeshHit hit;
+
+        NavMesh.SamplePosition(randDirection, out hit, dist, layerMask);
+
+        return hit.position;
+    }
+
+}
