4ef894ca5485063902ed96565a11ac739ad29578
Dylan Valkenaar <valkdylan@gmail.com>
2026-01-27 13:12:17 +0100

    Removed HUD on dialogue

--- Assets/Scripts/DisableIfDialogueIsRunning.cs
+++ Assets/Scripts/DisableIfDialogueIsRunning.cs
@@ -1,9 +1,11 @@
+using System.Collections.Generic;
 using UnityEngine;
 using Yarn.Unity;
 
 public class DisableIfDialogueIsRunning : MonoBehaviour
 {
     private DialogueRunner dr;
+    [SerializeField] private List<GameObject> objectsToDeactivate;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Awake()
     {
@@ -13,13 +15,20 @@ public class DisableIfDialogueIsRunning : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Debug.Log(dr.IsDialogueRunning);
         if (dr.IsDialogueRunning)
         {
-            gameObject.SetActive(false);
+            foreach (GameObject go in objectsToDeactivate)
+            {
+                go.SetActive(false);
+            }
         }
         else
         {
-            gameObject.SetActive(true);
+            foreach (GameObject go in objectsToDeactivate)
+            {
+                go.SetActive(true);
+            }
         }
     }
 }
