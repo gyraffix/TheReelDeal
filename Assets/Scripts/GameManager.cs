c7f2657d9ce66c695794be051f151ce2aa2ba05d
Dylan Valkenaar <valkdylan@gmail.com>
2026-01-28 17:19:24 +0100

    Changed Build Settings

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,7 +2,6 @@ using NUnit.Framework;
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.Rendering;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using Yarn.Unity;
